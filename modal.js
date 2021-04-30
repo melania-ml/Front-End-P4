8549e70c7a98d02363e45931e472f0c98fde36e2
melania-ml <melania.mlopez@gmail.com>
2021-04-30 11:40:02 +0200

    add functions, change focus color, change confirmation style

--- modal.js
+++ modal.js
@@ -1,3 +1,9 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll(".close") 
+
 // edit nav
 function editNav() {
   var x = document.getElementById("myTopnav");
@@ -7,11 +13,6 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -23,60 +24,196 @@ function launchModal() {
 }
 
 // close window
-function close(){
+closeModalBtn[0].addEventListener("click", closeModal);
+function closeModal(){
   modalbg.style.display="none";
 }
 
 window.onclick = function(e){
-  if (e.target === bground){
-    bground.style.display="none";
+  if (e.target === modalbg){
+    modalbg.style.display="none";
   }
 }
 
 window.addEventListener("keydown", function(event){
   if (event.key === "Escape"){
-    bground.style.display="none";
+    modalbg.style.display="none";
   }
 })
+
 
 // DOM elements form validation
 let firstName = document.getElementById('first');
 let lastName = document.getElementById('last');
 let email = document.getElementById('email');
-let birthday = document.getElementById('birthday');
+let birthdate = document.getElementById('birthdate');
 let quantity = document.getElementById('quantity');
 let locationTotal = document.getElementById('locationTotal');
+let locations = document.querySelectorAll('#locationTotal .checkbox-input');
 let checkbox1 = document.getElementById('checkbox1');
+let input = document.getElementsByClassName('text-control');
 let form = document.getElementById('form');
-let regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+const reletter = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+const remail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // FORM FIELD VALIDATION //
 
 // NAME
 function validateFirstName() {
   if (firstName.value.trim().length < 2 || 
-  first.value.trim() === '' || 
-  !firstName.value.match(regex)) {
+  firstName.value.trim() === '' || 
+  !firstName.value.match(reletter)) {
       firstName.parentElement.setAttribute('data-error-visible', 'true');
       firstName.style.border = '2px solid #e54858';
       return false;
   }
-  first.parentElement.setAttribute('data-error-visible', 'false');
+  firstName.parentElement.setAttribute('data-error-visible', 'false');
   firstName.style.border = '2px solid #279e7a';
   return true;
 }
 
-// FIELDS VALIDATION
-function formValidation() {
-  if (validateFirstName() === true) {
-      return true;
+// LASTNAME
+function validateLastName() {
+  if (lastName.value.trim().length < 2 || 
+  lastName.value.trim() === '' || 
+  !lastName.value.match(reletter)) {
+      lastName.parentElement.setAttribute('data-error-visible', 'true');
+      lastName.style.border = '2px solid #e54858';
+      return false;
   }
-  return false;
+  lastName.parentElement.setAttribute('data-error-visible', 'false');
+  lastName.style.border = '2px solid #279e7a';
+  return true;
+}
+
+// EMAIL
+function validateEmail() {
+    if (email.value.trim().match(remail)) {
+        email.parentElement.setAttribute('data-error-visible', 'false');
+        email.style.border = '2px solid #279e7a';
+        return true;
+    }
+    email.parentElement.setAttribute('data-error-visible', 'true');
+    email.style.border = '2px solid #e54858';
+    return false;
+}
+
+// BIRTHDATE
+function validateBirthdate() {
+    if (birthdate.value.trim().length !== 10) {
+        birthdate.parentElement.setAttribute('data-error-visible', 'true');
+        birthdate.style.border = '2px solid #e54858';
+        return false;
+    }
+    birthdate.parentElement.setAttribute('data-error-visible', 'false');
+    birthdate.style.border = '2px solid #279e7a';
+    return true;
+}
+
+// NUMBER OF TOURNAMENTS
+function validateNumberTournaments() {
+    if (quantity.value.trim().length === 0 || isNaN(quantity.value.trim()) === true || quantity.value.trim() < 0) {
+        quantity.parentElement.setAttribute('data-error-visible', 'true');
+        quantity.style.border = '2px solid #e54858';
+        return false;
+    }
+    quantity.parentElement.setAttribute('data-error-visible', 'false');
+    quantity.style.border = '2px solid #279e7a';
+    return true;
+}
+
+// LOCATIONS
+function validateLocations() {
+    locationTotal.setAttribute('data-error-visible', 'true');
+    for (let i = 0; i < locations.length; i++) {
+        if (locations[i].checked) {
+            locationTotal.setAttribute('data-error-visible', 'false');
+            return true;
+        }
+    }
+    return false;
+}
+
+// TERMS AND CONDITIONS
+function validateCheckBox() {
+    if (checkbox1.checked === false) {
+        checkbox1.parentElement.setAttribute('data-error-visible', 'true');
+        return false;
+    }
+    checkbox1.parentElement.setAttribute('data-error-visible', 'false');
+    return true;
+}
+
+// FORM FIELDS EVENTS - FOCUS
+function formFieldsValidation(element, method, event) {
+    element.addEventListener(event, method);
+}
+formFieldsValidation(firstName, validateFirstName, 'focusout');
+formFieldsValidation(lastName, validateLastName, 'focusout');
+formFieldsValidation(email, validateEmail, 'focusout');
+formFieldsValidation(birthdate, validateBirthdate, 'focusout');
+formFieldsValidation(quantity, validateNumberTournaments, 'focusout');
+formFieldsValidation(locationTotal, validateLocations, 'change');
+formFieldsValidation(checkbox1, validateCheckBox, 'change');
+
+
+// ALL FIELDS VALIDATION
+function allFieldsValidation() {
+    validateFirstName()
+    validateLastName()
+    validateEmail()
+    validateBirthdate()
+    validateNumberTournaments()
+    validateLocations()
+    validateCheckBox()
+}
+
+function formValidation() {
+    if (validateFirstName() === true &&
+        validateLastName() === true &&
+        validateEmail() === true &&
+        validateBirthdate() === true &&
+        validateNumberTournaments() === true &&
+        validateLocations() === true &&
+        validateCheckBox() === true) {
+        return true;
+    }
+    return false;
 }
 
 // SEND FORM
-form.addEventListener('submit', function(e){
-  e.preventDefault();
-  if(formValidation()==true){
-  }
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (formValidation() == true) {
+        displayModalSubmit();
+        document.querySelector('form').reset();
+    } else {
+        allFieldsValidation();
+    }
 });
+
+// SUBMIT CONFIRMATION
+let containerSubmit = document.getElementsByClassName('containerSubmit');
+let closeSubmit = document.getElementsByClassName('closeSubmit');
+let closeConfirmation = document.getElementById('closeConfirmation');
+
+
+// DISPLAY MODAL SUBMIT
+function displayModalSubmit() {
+    modalbg.style.display = 'none';
+    containerSubmit[0].style.display = 'block';
+}
+
+// CLOSE 
+function close() {
+    containerSubmit[0].style.display = 'none';
+    first.style.border = 'none';
+    last.style.border = 'none';
+    email.style.border = 'none';
+    birthdate.style.border = 'none';
+    quantity.style.border = 'none';
+}
+
+// EVENT CLOSE MODAL SUBMIT
+closeSubmit[0].addEventListener('click', close);
+closeConfirmation.addEventListener('click', close);
